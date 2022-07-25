05ee47c4e14b9038cc5e39a51303282d16603237
taimaskhanov11 <81189712+taimaskhanov11@users.noreply.github.com>
2022-07-25 14:01:54 +0300

    update purchase excepting

--- autoanswer/apps/bot/utils/purchase.py
+++ autoanswer/apps/bot/utils/purchase.py
@@ -9,20 +9,24 @@ from autoanswer.loader import bot, _
 
 async def checking_purchases():
     logger.trace("Checking purchases")
-    for cls in [InvoiceCrypto, InvoiceQiwi, InvoiceYooKassa]:
-        logger.trace(f"Check cls {cls.__name__}")
-        invoices: list[InvoiceCrypto | InvoiceQiwi, InvoiceYooKassa] = await cls.filter(
-            expire_at__gte=datetime.datetime.now(TZ),
-            is_paid=False)
-        for invoice in invoices:
-            logger.trace(f"Check invoice {invoice.invoice_id}[{invoice.amount}]")
-            try:
-                if await invoice.check_payment():
-                    await invoice.successfully_paid()
-                    logger.success(
-                        f"The invoice [{cls.__name__}] [{invoice.user}]{invoice.amount} {invoice.currency} "
-                        f"has been successfully paid")
-                    await bot.send_message(invoice.user.user_id,
-                                           _("✅ Подписка {} успешно оплачена").format(invoice.subscription_template))
-            except Exception as e:
-                logger.warning(e)
+    try:
+        for cls in [InvoiceCrypto, InvoiceQiwi, InvoiceYooKassa]:
+            logger.trace(f"Check cls {cls.__name__}")
+            invoices: list[InvoiceCrypto | InvoiceQiwi, InvoiceYooKassa] = await cls.filter(
+                expire_at__gte=datetime.datetime.now(TZ),
+                is_paid=False)
+            for invoice in invoices:
+                logger.trace(f"Check invoice {invoice.invoice_id}[{invoice.amount}]")
+                try:
+                    if await invoice.check_payment():
+                        await invoice.successfully_paid()
+                        logger.success(
+                            f"The invoice [{cls.__name__}] [{invoice.user}]{invoice.amount} {invoice.currency} "
+                            f"has been successfully paid")
+                        await bot.send_message(invoice.user.user_id,
+                                               _("✅ Подписка {} успешно оплачена").format(
+                                                   invoice.subscription_template))
+                except Exception as e:
+                    logger.warning(e)
+    except Exception as e:
+        logger.error(e)
